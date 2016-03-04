bf7e1b3940253b1264fa4b3889c3759e2bb42ef6
mirian <roxsis18@gmail.com>
2016-03-03 22:34:48 -0500

    iniciando trabajo

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+<div>	
+<nav>
+	<ul class="menu-lista">
+		<li class="menu-item"><a href="" class="menu-link">INICIO</a></li>
+		<li class="menu-item"><a href="" class="menu-link">GALERIA</a></li>
+		<li class="menu-item"><a href="" class="menu-link">BLOG</a></li>
+		<li class="menu-item"><a href="" class="menu-link">ANUNCIOS</a></li>
+		<li class="menu-item"><a href="" class="menu-link">CONTÁCTENOS</a></li>
+	</ul>
+</nav>
+<div class="estilo.css">
+<<img src="1.jpg" height="600" width="2000">
+<<img src="2.jpg" height="350" width="302">
+<<img src="3.jpg" height="350" width="302">
+<<img src="4.jpg" height="350" width="302">
+<<img src="5.jpg" height="350" width="302">
+</body>
+</html>
